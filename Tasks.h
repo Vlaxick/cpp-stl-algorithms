faaddfecf5a2f81573a8e848f0e1340a2d3e64e2
Macbook <macbook@Macbooks-MacBook-Air.local>
2026-05-04 21:23:23 +0300

    Add generic displayContainer template function to Tasks.h

--- Tasks.h
+++ Tasks.h
@@ -1,6 +1,15 @@
 #pragma once
 #include <iostream>
 
+template<typename T>
+void displayContainer(const T& container, std::string header) {
+    std::cout << header;
+    for (const auto& element : container) {
+        std::cout << element << " ";
+    }
+    std::cout << std::endl;
+}
+
 bool isOdd(int number) {
     return number % 2 != 0;
 }
